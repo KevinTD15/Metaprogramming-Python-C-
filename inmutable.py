a83307fa9ba2882f5f4c2f3dd06a72c96540fa21
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-10 08:21:10 -0500

    arreglado nombre

--- inmutable.py
+++ inmutable.py
@@ -1,7 +1,7 @@
 import threading
 
 
-class ImmutableMeta(type):
+class InmutableMeta(type):
     # nombres mangleados con __ para simular comportamiento privado
     __attrModdifiers = {} # aqui almacenaremos tuplas de las dos funciones que modifican atributos de una clase
     __lock = threading.Lock() # utilizaremos locks para evitar condiciones de carrera
@@ -10,15 +10,15 @@ class ImmutableMeta(type):
         raise AttributeError("Attribute Modifications are not allowed")
 
     def __call__(cls, *args, **kwargs):
-        with ImmutableMeta.__lock:
+        with InmutableMeta.__lock:
             # guardamos los modificadores de atributos en nuestro diccionario
             if cls not in cls.__attrModdifiers:
-                ImmutableMeta.__attrModdifiers[cls] = cls.__setattr__, cls.__delattr__
+                InmutableMeta.__attrModdifiers[cls] = cls.__setattr__, cls.__delattr__
             else:
                 # restauramos los modificadores dummy para que las instanciaciones añadan sus propiedades
-                cls.__setattr__,cls.__delattr__ = ImmutableMeta.__attrModdifiers[cls]
+                cls.__setattr__,cls.__delattr__ = InmutableMeta.__attrModdifiers[cls]
             #instanciamos llamando al call de type q llamara al new y al init de la clase
-            instance = super(ImmutableMeta, cls).__call__(*args, **kwargs)
+            instance = super(InmutableMeta, cls).__call__(*args, **kwargs)
             # sustituimos los modifficadores por una funcion dummy que arroje excepcion
-            cls.__setattr__ = cls.__delattr__ = ImmutableMeta.__error_notifier
+            cls.__setattr__ = cls.__delattr__ = InmutableMeta.__error_notifier
             return instance
